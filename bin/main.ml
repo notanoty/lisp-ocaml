2c07a6de791a0c8a3a0e7842e7c30d2abaa9f9e7
notanoty <timariefev05@gmail.com>
2024-06-01 14:47:22 +0200

    I dont rememember

--- bin/main.ml
+++ bin/main.ml
@@ -2,48 +2,46 @@
 [@@@ocaml.warning "-33"]
 
 open Peano
-open Tokenize 
-open Parsing 
+open Tokenize
+open Parsing
 open Mylisplang
 
 let dir_location = "codefolder/"
-
 let file_standard = "testcode.ls"
 (* open Language.Rutalg *)
 (* exception Error of string *)
 
-let rec conc ls1 ls2 = 
+let rec conc ls1 ls2 =
   match (ls1, ls2) with
-  | ([], ls2) -> ls2
-  | ( (head :: tail), ls2 ) -> head ::(  conc tail  (ls2) )
+  | [], ls2 -> ls2
+  | head :: tail, ls2 -> head :: conc tail ls2
 
-let read_file file =
-  In_channel.with_open_bin file In_channel.input_all
+let read_file file = In_channel.with_open_bin file In_channel.input_all
+let test = S_expr (String "+", S_expr (Int 7, S_expr (Int 1, Nil)))
+let nil_test = S_expr (Nil, Nil)
+let array = [ 1; 2; 3; 4 ]
+let example_small_cond = "(cond ( (null (list)) (+ 1 2)) ( t (list 2 3 4 5)) ) "
 
-let test = S_expr ((String "+"), (S_expr ((Int 7), (S_expr (Int 1, Nil) )) ));;  
+let example_big_cond =
+  "( cond ( (null (list  1 2 3 )) (+ 1 1) ) ( (null (list 2 3 )) 5  )  (t (+ 1 \
+   0) ) ) "
 
-let nil_test = S_expr ( Nil, Nil);; 
+let example_null = "(null (list ) ) "
 
-let array = [1;2;3;4];;
-let example_small_cond = "(cond ( (null (list)) (+ 1 2)) ( t (list 2 3 4 5)) ) " ;;  
-let example_big_cond = "( cond ( (null (list  1 2 3 )) (+ 1 1) ) ( (null (list 2 3 )) 5  )  (t (+ 1 0) ) ) ";;  
-let example_null = "(null (list ) ) " ;; 
- 
-
-let get_contents_from_file = 
+let get_contents_from_file =
   (* Printf.printf "Number of arguments: %d\n" (Array.length Sys.argv - 1); *)
-  read_file ( dir_location ^ if Array.length Sys.argv - 1 == 0 then file_standard else Sys.argv.(1))
+  read_file
+    (dir_location
+    ^ if Array.length Sys.argv - 1 == 0 then file_standard else Sys.argv.(1))
 
-let () = 
-  if(Array.length Sys.argv - 1 > 0 ) then
-    (
+let () =
+  if Array.length Sys.argv - 1 > 0 then
     let contents = "(" ^ get_contents_from_file ^ ")" in
     (* Printf.printf  "%s\n" contents; *)
     let parsing_result = parsing contents in
     driver_loop parsing_result Nil
-    )
-  else ignore(driver_loop_terminal Nil);;
+  else ignore (driver_loop_terminal Nil)
 
 (* let example = "( (lambda (x y) ( + ((lambda (x y) (+ x y) ) 1 2 ) (+ x y) ) ) (+ 3 1) 4)" in *)
-   (* let  a = parsing example in *)
-    (* print_exsprassion (eval a); *)
+(* let  a = parsing example in *)
+(* print_exsprassion (eval a); *)
--- lib/peano/peano.ml
+++ lib/peano/peano.ml
@@ -1,161 +1,156 @@
 (* [@@@ocaml.warning "-8"] *)
-type peano_number = Zero | Successor of peano_number;;
+type peano_number = Zero | Successor of peano_number
 
+type sexpr =
+  | Nil
+  | Int of int
+  | String of string
+  | S_expr of sexpr  * sexpr 
+  | Closure of sexpr  * sexpr 
+(*
+let exp =
+  ref (S_expr
+    ( ref (S_expr ( ref (Int 10), ref (S_expr ( ref (String "test"), ref( Int 1))))),
+      ref (S_expr ( ref (String "aaaa"), ref (Int 44))) )) *)
 
-type sexpr = 
-    | Nil 
-    | Int of int
-    | String of string
-    | S_expr of sexpr * sexpr
-    | Closure of sexpr * sexpr;;
+let test = S_expr (String "+", S_expr (Int 1, Int 1))
 
-
-
-let exp = S_expr (S_expr( (Int 10), (S_expr ((String "test") , (Int 1) ) ) ), S_expr ((String "aaaa") , (Int 44)) ) ;;
-let test = S_expr ((String "+"), (S_expr ((Int 1), (Int 1)) ));;  
- (* поддержка под выражений  *)
-
+(* поддержка под выражений  *)
 
 (* (1 2 3 4) (1 . (2 . (3 . (4 . nil)))) *)
 let print_exsprassion exsprassion =
-  let rec print_exp_par =  function
-          | Nil -> Printf.printf "Nil"
-          | Int x -> Printf.printf "%d " x 
-          | String x ->  Printf.printf "'%s' " x        
-          | S_expr (x, (S_expr (y,z ) )) ->   
-                              Printf.printf "( ";
-                              print_exp_par x;
-                              print_exp (S_expr (y,z));
-                              Printf.printf ") "
-          | S_expr (x,  Nil) ->  
-                              Printf.printf "( ";
-                              print_exp_par x;
-                              Printf.printf ") "
-
-          | S_expr (x,  y) ->  
-                              Printf.printf "( ";
-                              print_exp_par x;
-                              Printf.printf ". ";
-                              print_exp y ;
-                              Printf.printf ") "
-    | Closure _ -> Printf.printf "Closure" 
-
-     and print_exp = function
-          | Nil -> Printf.printf "test "
-          | Int x -> Printf.printf "%d " x 
-          | String x ->  Printf.printf "'%s' " x
-          | S_expr (x, (S_expr (y, z)) ) ->  
-                              print_exp_par x;
-                              print_exp (S_expr (y,z))
-          | S_expr (x,  Nil) ->  
-                              print_exp_par x;
-          | S_expr (x, y) ->  
-                              print_exp_par x;
-                              Printf.printf ". ";
-                              print_exp  y 
-          | Closure _ -> Printf.printf "Closure" 
-      in
-  print_exp_par exsprassion ;
-  Printf.printf "\n";;
+  let rec print_exp_par = function
+    | Nil -> Printf.printf "Nil"
+    | Int x -> Printf.printf "%d " x
+    | String x -> Printf.printf "'%s' " x
+    | S_expr (x, S_expr (y, z)) ->
+        Printf.printf "( ";
+        print_exp_par x;
+        print_exp (S_expr (y, z));
+        Printf.printf ") "
+    | S_expr (x, Nil) ->
+        Printf.printf "( ";
+        print_exp_par x;
+        Printf.printf ") "
+    | S_expr (x, y) ->
+        Printf.printf "( ";
+        print_exp_par x;
+        Printf.printf ". ";
+        print_exp y;
+        Printf.printf ") "
+    | Closure _ -> Printf.printf "Closure"
+  and print_exp = function
+    | Nil -> Printf.printf "test "
+    | Int x -> Printf.printf "%d " x
+    | String x -> Printf.printf "'%s' " x
+    | S_expr (x, S_expr (y, z)) ->
+        print_exp_par x;
+        print_exp (S_expr (y, z))
+    | S_expr (x, Nil) -> print_exp_par x
+    | S_expr (x, y) ->
+        print_exp_par x;
+        Printf.printf ". ";
+        print_exp y
+    | Closure _ -> Printf.printf "Closure"
+  in
+  print_exp_par exsprassion;
+  Printf.printf "\n"
 
 let print_exsprassion_full exsprassion =
-  let rec print_exp_par =  function
-          | Nil -> Printf.printf "Nil"
-          | Int x -> Printf.printf "Int %d " x 
-          | String x ->  Printf.printf "String \"%s\" " x
-          | S_expr (x, ( S_expr (y, z) )) ->  
-                              Printf.printf "S_expr (";
-                              Printf.printf "(";
-                              print_exp_par x;
-                              Printf.printf "), (";
-                              print_exp_par ( S_expr (y, z) ) ;
-                              Printf.printf ")) ";
-          | S_expr (x,  y) ->  
-                              Printf.printf "S_expr (";
-                              print_exp_par x;
-                              Printf.printf ", ";
-                              print_exp_par y ;
-                              Printf.printf ") ";
-          | Closure _ -> Printf.printf "(Closure)" 
-      in
-  print_exp_par exsprassion ;
-  Printf.printf "\n";;
-
+  let rec print_exp_par = function
+    | Nil -> Printf.printf "Nil"
+    | Int x -> Printf.printf "Int %d " x
+    | String x -> Printf.printf "String \"%s\" " x
+    | S_expr (x, S_expr (y, z)) ->
+        Printf.printf "S_expr (";
+        Printf.printf "(";
+        print_exp_par x;
+        Printf.printf "), (";
+        print_exp_par (S_expr (y, z));
+        Printf.printf ")) "
+    | S_expr (x, y) ->
+        Printf.printf "S_expr (";
+        print_exp_par x;
+        Printf.printf ", ";
+        print_exp_par y;
+        Printf.printf ") "
+    | Closure _ -> Printf.printf "(Closure)"
+  in
+  print_exp_par exsprassion;
+  Printf.printf "\n"
 
 let rec exp_to_string = function
-        | Nil -> "Nil "
-        | Int x -> Printf.sprintf "%d " x 
-        | String x ->  Printf.sprintf "\"%s\" " x
-        | S_expr (x, (S_expr (y,z ) )) ->   "( " ^ exp_to_string x ^ exp_to_string_no_par (S_expr (y,z)) ^ ") "
-        | S_expr (x,  Nil) ->  "( " ^ exp_to_string x ^ ") "
-        | S_expr (x,  y) ->  "( " ^ exp_to_string x ^ ". " ^ exp_to_string_no_par y ^ ") "
-        | Closure _ -> "(Closure)" 
-        and  exp_to_string_no_par = function  
-        | Nil -> "Nil "
-        | Int x -> Printf.sprintf "%d " x 
-        | String x ->  Printf.sprintf "\"%s\" " x
-        | S_expr (x, (S_expr (y,z ) )) ->  exp_to_string x ^ exp_to_string_no_par (S_expr (y,z)) 
-        | S_expr (x,  Nil) ->  exp_to_string x 
-        | S_expr (x,  y) ->  exp_to_string x ^ ". " ^ exp_to_string_no_par y 
-        | Closure _ -> "Closure"
+  | Nil -> "Nil "
+  | Int x -> Printf.sprintf "%d " x
+  | String x -> Printf.sprintf "\"%s\" " x
+  | S_expr (x, S_expr (y, z)) ->
+      "( " ^ exp_to_string x ^ exp_to_string_no_par (S_expr (y, z)) ^ ") "
+  | S_expr (x, Nil) -> "( " ^ exp_to_string x ^ ") "
+  | S_expr (x, y) ->
+      "( " ^ exp_to_string x ^ ". " ^ exp_to_string_no_par y ^ ") "
+  | Closure _ -> "(Closure)"
+
+and exp_to_string_no_par = function
+  | Nil -> "Nil "
+  | Int x -> Printf.sprintf "%d " x
+  | String x -> Printf.sprintf "\"%s\" " x
+  | S_expr (x, S_expr (y, z)) ->
+      exp_to_string x ^ exp_to_string_no_par (S_expr (y, z))
+  | S_expr (x, Nil) -> exp_to_string x
+  | S_expr (x, y) -> exp_to_string x ^ ". " ^ exp_to_string_no_par y
+  | Closure _ -> "Closure"
 
 let rec get_exspression_length exsprassion =
   match exsprassion with
   | Nil -> 0
-  | Int _ -> 1 
-  | String _ -> 1 
-  | S_expr(_, next) -> 1 + (get_exspression_length next)
-  | Closure _ -> 1 
+  | Int _ -> 1
+  | String _ -> 1
+  | S_expr (_, next) -> 1 + get_exspression_length next
+  | Closure _ -> 1
 
-let rec peano_to_int = function
-  | Zero -> 0
-  | Successor x -> 1 + (peano_to_int x);;
+let rec peano_to_int = function Zero -> 0 | Successor x -> 1 + peano_to_int x
 
 let rec int_to_peano = function
   | 0 -> Zero
-  | x -> (Successor (int_to_peano (x - 1)  ));;
+  | x -> Successor (int_to_peano (x - 1))
 
-let rec add x y = 
-    match (x, y) with
-    | x, Zero -> x
-    | x,  Successor y -> add (Successor x) y;;
+let rec add x y =
+  match (x, y) with x, Zero -> x | x, Successor y -> add (Successor x) y
 
-let rec sub x y = 
-    match (x, y) with
-    | x, Zero -> x
-    | Zero, Successor _ -> Zero
-    | Successor x,  Successor y -> sub x y;;
+let rec sub x y =
+  match (x, y) with
+  | x, Zero -> x
+  | Zero, Successor _ -> Zero
+  | Successor x, Successor y -> sub x y
 
-let rec greater x y = 
-    match (x, y) with
-    | Zero, Zero -> false
-    | Successor _, Zero -> true
-    | Zero, Successor _ -> false
-    | Successor x, Successor y -> greater x y;;
+let rec greater x y =
+  match (x, y) with
+  | Zero, Zero -> false
+  | Successor _, Zero -> true
+  | Zero, Successor _ -> false
+  | Successor x, Successor y -> greater x y
 
+let rec less x y =
+  match (x, y) with
+  | Zero, Zero -> false
+  | Successor _, Zero -> false
+  | Zero, Successor _ -> true
+  | Successor x, Successor y -> less x y
 
-let rec less x y = 
-    match (x, y) with
-    | Zero, Zero -> false
-    | Successor _, Zero -> false
-    | Zero, Successor _ -> true
-    | Successor x, Successor y -> less x y;;
+let rec eq x y =
+  match (x, y) with
+  | Zero, Zero -> true
+  | Successor _, Zero -> false
+  | Zero, Successor _ -> false
+  | Successor x, Successor y -> eq x y
 
-let rec eq x y = 
-    match (x, y) with
-    | Zero, Zero -> true
-    | Successor _, Zero -> false
-    | Zero, Successor _ -> false
-    | Successor x, Successor y -> eq x y;;
-
-
-let rec list_string_to_exp list = 
-    match list with
-    | [] -> Nil
-    | head::[] -> S_expr ((String head),  Nil)
-    | head::tail -> S_expr ((String head),  list_string_to_exp tail)
+let rec list_string_to_exp list =
+  match list with
+  | [] -> Nil
+  | head :: [] -> S_expr (String head, Nil)
+  | head :: tail -> S_expr (String head, list_string_to_exp tail)
 
 (*  ("aasdas ", "bbbb". "cccc") *)
 
- (* S_expr (String "aasdas", (S_expr (String "bbbb",  Nil *)
-  (* (S_expr  (String "cccc", nil))) *)
+(* S_expr (String "aasdas", (S_expr (String "bbbb",  Nil *)
+(* (S_expr  (String "cccc", nil))) *)
