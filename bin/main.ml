03c5febf8e5fb8eb755595b38029a2b7f1c56ec9
notanoty <timariefev05@gmail.com>
2024-06-02 23:43:37 +0200

    code reformating started

--- bin/main.ml
+++ bin/main.ml
@@ -1,21 +1,42 @@
+[@@@ocaml.warning "-8"]
 [@@@ocaml.warning "-32"]
 [@@@ocaml.warning "-33"]
 
 open Peano
 open Tokenize
-open Parsing
-open Mylisplang
 
-let rec conc ls1 ls2 =
-  match (ls1, ls2) with
-  | [], ls2 -> ls2
-  | head :: tail, ls2 -> head :: conc tail ls2
+(* open Parsing *)
+(* open Mylisplang *)
+
+(* let exmple = ref (SS_expr (SInt (ref 1), SNil)) *)
+let int_expr = SInt { value = 42 }
+let string_expr = SString { value = "hello" }
+let s_expr = S_expr { current = int_expr; next = string_expr }
+let closure_expr = Closure { env = Nil; code = s_expr }
 
 let () =
-  if Array.length Sys.argv == 2 then (
-    let read_file file = In_channel.with_open_bin file In_channel.input_all in
-    let contents = "(" ^ read_file Sys.argv.(1) ^ ")" in
-    let parsing_result = parsing contents in
-    print_exsprassion parsing_result;
-    driver_loop parsing_result Nil)
-  else ignore (driver_loop_terminal Nil)
+  match s_expr with S_expr v -> v.current <- SInt { value = 50 } | _ -> ()
+;;
+
+(* s_expr.current <- string_expr;; *)
+
+(* let modify_current s_expr = *)
+(*   match s_expr with *)
+(*   | S_expr { current; next = _ } -> current := SInt { value = 22 } *)
+(*   | _ -> () (* Modifying mutable fields *) *)
+
+(* let () = match int_expr with SInt v -> v.value <- 43 | _ -> ();; *)
+
+print_exsprassion s_expr;;
+print_exsprassion_full s_expr;;
+Printf.printf "(%s)\n" (exp_to_string s_expr);;
+Printf.printf "len %d\n" (get_exspression_length s_expr)
+
+(* let () = *)
+(*   if Array.length Sys.argv == 2 then ( *)
+(*     let read_file file = In_channel.with_open_bin file In_channel.input_all in *)
+(*     let contents = "(" ^ read_file Sys.argv.(1) ^ ")" in *)
+(*     let parsing_result = parsing contents in *)
+(*     print_exsprassion parsing_result; *)
+(*     driver_loop parsing_result Nil) *)
+(*   else ignore (driver_loop_terminal Nil) *)
--- lib/peano/peano.ml
+++ lib/peano/peano.ml
@@ -5,74 +5,84 @@ type sexpr =
   | Nil
   | Int of int
   | String of string
-  | S_expr of sexpr  * sexpr 
-  | Closure of sexpr  * sexpr 
+  | S_expr of sexpr * sexpr
+  | Closure of sexpr * sexpr
+
+type s_expression =
+  | Nil
+  | SInt of { mutable value : int }
+  | SString of { mutable value : string }
+  | S_expr of { mutable current : s_expression; mutable next : s_expression }
+  | Closure of { mutable env : s_expression; mutable code : s_expression }
+
+let example = S_expr { current = SString { value = "aa" }; next = Nil }
 (*
 let exp =
   ref (S_expr
     ( ref (S_expr ( ref (Int 10), ref (S_expr ( ref (String "test"), ref( Int 1))))),
       ref (S_expr ( ref (String "aaaa"), ref (Int 44))) )) *)
 
-let test = S_expr (String "+", S_expr (Int 1, Int 1))
+(* let test = S_expr (String "+", S_expr (Int 1, Int 1)) *)
 
 (* поддержка под выражений  *)
 
 (* (1 2 3 4) (1 . (2 . (3 . (4 . nil)))) *)
-let print_exsprassion exsprassion =
+let print_exsprassion (exsprassion : s_expression) =
   let rec print_exp_par = function
     | Nil -> Printf.printf "Nil"
-    | Int x -> Printf.printf "%d " x
-    | String x -> Printf.printf "'%s' " x
-    | S_expr (x, S_expr (y, z)) ->
+    | SInt { value } -> Printf.printf "%d " value
+    | SString { value } -> Printf.printf "'%s' " value
+    | S_expr { current; next = S_expr { current = y; next = z } } ->
         Printf.printf "( ";
-        print_exp_par x;
-        print_exp (S_expr (y, z));
+        print_exp_par current;
+        print_exp (S_expr { current = y; next = z });
         Printf.printf ") "
-    | S_expr (x, Nil) ->
+    | S_expr { current; next = Nil } ->
         Printf.printf "( ";
-        print_exp_par x;
+        print_exp_par current;
         Printf.printf ") "
-    | S_expr (x, y) ->
+    | S_expr { current; next } ->
         Printf.printf "( ";
-        print_exp_par x;
+        print_exp_par current;
         Printf.printf ". ";
-        print_exp y;
+        print_exp next;
         Printf.printf ") "
     | Closure _ -> Printf.printf "Closure"
   and print_exp = function
-    | Nil -> Printf.printf "test "
-    | Int x -> Printf.printf "%d " x
-    | String x -> Printf.printf "'%s' " x
-    | S_expr (x, S_expr (y, z)) ->
-        print_exp_par x;
-        print_exp (S_expr (y, z))
-    | S_expr (x, Nil) -> print_exp_par x
-    | S_expr (x, y) ->
-        print_exp_par x;
+    | Nil -> Printf.printf "Nil"
+    | SInt { value } -> Printf.printf "%d " value
+    | SString { value } -> Printf.printf "'%s' " value
+    | S_expr { current; next = S_expr { current = y; next = z } } ->
+        print_exp_par current;
+        print_exp (S_expr { current = y; next = z })
+    | S_expr { current; next = Nil } -> print_exp_par current
+    | S_expr { current; next } ->
+        print_exp_par current;
         Printf.printf ". ";
-        print_exp y
+        print_exp next
     | Closure _ -> Printf.printf "Closure"
   in
+
   print_exp_par exsprassion;
   Printf.printf "\n"
 
-let print_exsprassion_full exsprassion =
+let print_exsprassion_full (exsprassion : s_expression) =
   let rec print_exp_par = function
     | Nil -> Printf.printf "Nil"
-    | Int x -> Printf.printf "Int %d " x
-    | String x -> Printf.printf "String \"%s\" " x
-    | S_expr (x, S_expr (y, z)) ->
+    | SInt { value } -> Printf.printf "Int %d " value
+    | SString { value } -> Printf.printf "String \"%s\" " value
+    | S_expr { current; next = S_expr { current = y; next = z } } ->
         Printf.printf "S_expr (";
         Printf.printf "(";
-        print_exp_par x;
+        print_exp_par current;
         Printf.printf "), (";
-        print_exp_par (S_expr (y, z));
+        print_exp_par (S_expr { current = y; next = z });
         Printf.printf ")) "
-    | S_expr (x, y) ->
+    | S_expr { current; next } ->
         Printf.printf "S_expr (";
-        print_exp_par x;
+        print_exp_par current;
         Printf.printf ", ";
-        print_exp_par y;
+        print_exp_par next;
         Printf.printf ") "
     | Closure _ -> Printf.printf "(Closure)"
   in
@@ -81,31 +91,36 @@ let print_exsprassion_full exsprassion =
 
 let rec exp_to_string = function
   | Nil -> "Nil "
-  | Int x -> Printf.sprintf "%d " x
-  | String x -> Printf.sprintf "\"%s\" " x
-  | S_expr (x, S_expr (y, z)) ->
-      "( " ^ exp_to_string x ^ exp_to_string_no_par (S_expr (y, z)) ^ ") "
-  | S_expr (x, Nil) -> "( " ^ exp_to_string x ^ ") "
-  | S_expr (x, y) ->
-      "( " ^ exp_to_string x ^ ". " ^ exp_to_string_no_par y ^ ") "
+  | SInt { value } -> Printf.sprintf "%d " value
+  | SString { value } -> Printf.sprintf "\"%s\" " value
+  | S_expr { current; next = S_expr { current = y; next = z } } ->
+      "( " ^ exp_to_string current
+      ^ exp_to_string_no_par (S_expr { current = y; next = z })
+      ^ ") "
+  | S_expr { current; next = Nil } -> "( " ^ exp_to_string current ^ ") "
+  | S_expr { current; next } ->
+      "( " ^ exp_to_string current ^ ". " ^ exp_to_string_no_par next ^ ") "
   | Closure _ -> "(Closure)"
 
 and exp_to_string_no_par = function
   | Nil -> "Nil "
-  | Int x -> Printf.sprintf "%d " x
-  | String x -> Printf.sprintf "\"%s\" " x
-  | S_expr (x, S_expr (y, z)) ->
-      exp_to_string x ^ exp_to_string_no_par (S_expr (y, z))
-  | S_expr (x, Nil) -> exp_to_string x
-  | S_expr (x, y) -> exp_to_string x ^ ". " ^ exp_to_string_no_par y
+  | SInt { value } -> Printf.sprintf "%d " value
+  | SString { value } -> Printf.sprintf "\"%s\" " value
+  | S_expr { current; next = S_expr { current = y; next = z } } ->
+      (* | S_expr (x, S_expr (y, z)) -> *)
+      exp_to_string current
+      ^ exp_to_string_no_par (S_expr { current = y; next = z })
+  | S_expr { current; next = Nil } -> exp_to_string current
+  | S_expr { current; next } ->
+      exp_to_string current ^ ". " ^ exp_to_string_no_par next
   | Closure _ -> "Closure"
 
-let rec get_exspression_length exsprassion =
+let rec get_exspression_length (exsprassion : s_expression) : int =
   match exsprassion with
   | Nil -> 0
-  | Int _ -> 1
-  | String _ -> 1
-  | S_expr (_, next) -> 1 + get_exspression_length next
+  | SInt _ -> 1
+  | SString _ -> 1
+  | S_expr { current = _; next } -> 1 + get_exspression_length next
   | Closure _ -> 1
 
 let rec peano_to_int = function Zero -> 0 | Successor x -> 1 + peano_to_int x
@@ -144,11 +159,13 @@ let rec eq x y =
   | Zero, Successor _ -> false
   | Successor x, Successor y -> eq x y
 
-let rec list_string_to_exp list =
-  match list with
+let rec list_string_to_exp list_args : s_expression =
+  match list_args with
   | [] -> Nil
-  | head :: [] -> S_expr (String head, Nil)
-  | head :: tail -> S_expr (String head, list_string_to_exp tail)
+  | head :: [] -> S_expr { current = SString { value = head }; next = Nil }
+  | head :: tail ->
+      S_expr
+        { current = SString { value = head }; next = list_string_to_exp tail }
 
 (*  ("aasdas ", "bbbb". "cccc") *)
 
